7e424736ee854ff8cc79fe7cdcfcb0b8cd591d31
Kushagr Mittal <kushagr.mittal@live.com>
2021-03-08 19:48:32 +1100

    Fix some data issues

--- helpers.py
+++ helpers.py
@@ -7,5 +7,5 @@ from typing import Tuple
 def get_timezone_details() -> Tuple[tzinfo, datetime]:
     timezone: tzinfo = dateutil.tz.gettz(environ.get('TIMEZONE_NAME')) or dateutil.tz.UTC
     today: datetime = datetime.now(tz=timezone) 
-    # today: datetime = datetime(2021, 3, 1, tzinfo=timezone) 
+    # today: datetime = datetime(2021, 3, 9, tzinfo=timezone) 
     return timezone, today
